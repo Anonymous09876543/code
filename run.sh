6378cc19a5c756b0f7ed4abb9d023512ee35b990
Anonymous09876543 <128361860+Anonymous09876543@users.noreply.github.com>
2023-03-23 22:01:12 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -e result/exp-his.err
-#SBATCH -o result/exp-his.out
-#SBATCH -J prefix-tuning
-#SBATCH --partition=si
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=999:00:00
-python train.py --cfg configs/models/regression_metric.yaml

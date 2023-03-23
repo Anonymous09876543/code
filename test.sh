05b06d426162057c36dfb6f0b526b3a3988cf8e5
Anonymous09876543 <128361860+Anonymous09876543@users.noreply.github.com>
2023-03-23 22:01:20 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -e test/exp-his.err
-#SBATCH -o test/exp-his.out
-#SBATCH -J test
-#SBATCH --partition=si
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=999:00:00
-python score.py
